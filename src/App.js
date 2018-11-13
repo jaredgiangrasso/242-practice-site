80a42da8045fc7cb9d4f95d95b496ada7d4dd6d1
Jared Giangrasso <jared@Jareds-Mac-mini.fios-router.home>
2018-11-13 10:08:38 -0500

    add basic navigation, heading, and styling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>242 Practice Site</h1>
+      <div className='App'>
+        < Navigation />
+        < Header />
+      </div>
     );
   }
 }
+
+const Header = () => 
+  <div className='container'>
+    <div className='row'>
+      <div className='col-lg-12'>
+        <h1>Heading</h1>
+      </div>
+    </div>
+  </div>
+
+const Navigation = () =>
+  <nav className="navbar justify-content-center navbar-fixed-top navbar-expand-lg">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Lorem</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Ipsum</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-item nav-link" href="#">Dolor</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Elit</a>
+      </li>
+    </ul>
+  </nav>
+
 
 export default App;
